93f652085e8e85b831a0ea12a9187761eef21cd8
Eduarda <lourencoeduarda.m@gmail.com>
2024-12-12 16:00:53 -0300

    adicionando o react

--- produtos_api/serializers.py
+++ produtos_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Produto
 class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Produto
-        fields = '__all__' 
+         fields = '__all__' 
